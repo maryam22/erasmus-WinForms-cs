78fe7e4115f6d7f7c3a7e1646407a5f6b9de2318
maryam22 <maryambar22@gmail.com>
2021-10-18 15:44:51 +0200

    Add project files.

---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void show(object sender, EventArgs e)
+        {
+
+            var serializer = new System.Xml.Serialization.XmlSerializer(typeof(PurchaseOrder));
+            var stream = new FileStream("purchaseorder.xml", FileMode.Open, FileAccess.Read);
+
+            PurchaseOrder? purchaseorder = (PurchaseOrder?)serializer.Deserialize(stream);
+
+            purchaseorder.PurchaseOrderNumber = "99504";
+
+            using (var outputstream = new FileStream("result.xml", FileMode.Create, FileAccess.Write))
+            {
+                using (stream)
+                {
+                    var bItems = purchaseorder.Items.Where(i => i.PartNumber.Contains("A"));
+                    serializer.Serialize(outputstream, purchaseorder);
+
+
+                    MessageBox.Show($"result is {string.Join(", ", bItems.Select(b => b.PartNumber))}");
+                }
+            }
+            
+        }
+    }
+}
---
+++ WinFormsApp3/PurchaseOrder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WinFormsApp3
+{
+
+    [XmlRoot("purchase-order")]
+    public class PurchaseOrder
+    {
+        [XmlAttribute("PurchaseOrderNumber")]
+        public string PurchaseOrderNumber { get; set; }
+        [XmlElement("Address")]
+        public Address Address { get; set; }
+        [XmlArray("Items"), XmlArrayItem("Item")]
+        public List<Item> Items { get; set; }
+    }
+
+    public class Address
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+        [XmlElement("Street")]
+        public string Street { get; set; }
+        [XmlElement("City")]
+        public string City { get; set; }
+        [XmlElement("State")]
+        public string State { get; set; }
+        [XmlElement("Zip")]
+        public string Zip { get; set; }
+        [XmlElement("Country")]
+        public string Country { get; set; }
+    }
+
+    public class Item
+    {
+        [XmlAttribute("PartNumber")]
+        public string PartNumber { get; set; }
+
+        [XmlElement("ProductName")]
+        public string ProductName { get; set; }
+    }
+
+
+} 
+
+
+
